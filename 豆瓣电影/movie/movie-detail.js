c1ce6383fbb3599a732c1381102a6ff1db14d67d
liujinger <1138056236@qq.com>
2016-12-29 15:36:08 +0800

    movies

---
+++ 豆瓣电影/movie/movie-detail.js
@@ -0,0 +1,28 @@
+/**
+ * Created by wenyuan on 2016/11/22.
+ */
+
+/*
+* 数据解析 配置到界面上 
+* */
+
+(function () {
+    var detailModule = angular.module('app.movieDetail',[]);
+    detailModule.controller('movieDetailController',function ($scope,$rootScope,URLConfig,$routeParams,$http) {
+
+        var movieId = $routeParams.movieId;
+        var fullUrl = URLConfig.appURL + 'subject/' + movieId + '?callback=movieDetailCallBack';
+
+        $http.jsonp(fullUrl).success(function () {
+            console.log("请求成功");
+        }).error(function () {
+            console.log("请求失败");
+        });
+
+        window.movieDetailCallBack = function (jsonData) {
+            $scope.movie = jsonData;
+        };
+
+        $rootScope.navTitle = "电影详情";
+    })
+}())
